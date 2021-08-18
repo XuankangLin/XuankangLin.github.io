c174e5e019a3da21e5698212c75e0345d337aefd
Xuankang Lin <xuankang.lin@gmail.com>
2021-08-18 18:40:09 -0400

    Site updated: 2021-08-18 18:40:07

---
+++ js/gtag.js
@@ -0,0 +1,10 @@
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<!-- id hardcoded -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-42282594-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-42282594-1');
+</script>
